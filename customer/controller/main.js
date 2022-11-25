da01cb09bc784021cde062e9c17c4d3f5bf95442
Trung <trung.nt.e@gmail.com>
2022-11-26 01:40:57 +0700

    Add item to cart

--- customer/controller/main.js
+++ customer/controller/main.js
@@ -1,5 +1,6 @@
 //  global variable
 var listProduct = [];
+var listCartItem = [];
 
 // declare a global variable for Product List
 var productService = new ProductService();
@@ -43,7 +44,7 @@ function displayProducts(arrayProduct) {
                 <div class="card-body">
                     <h5 class="card-title">${product.name}</h5>
                     <p class="card-text product__price">${product.price.toLocaleString()}$</p>
-                    <a href="#" class="btn btn-primary">Add to cart</a>
+                    <a href="#" class="btn btn-primary" onclick="addToCart(${product.id})">Add to cart</a>
                 </div>
             </div>
         </div>
@@ -77,8 +78,57 @@ function filterProduct() {
     displayProducts(arrayFilterPorduct);
 }
 
-
-
-
 //  entry program
 getListProducts();
+
+
+// ------------------------------------------------------
+// Cart handler
+// ------------------------------------------------------
+// find product by id
+function findProductById(id) {
+    for (var index = 0; index < listProduct.length; index++) {
+        if(id === Number(listProduct[index].id)) {
+            return listProduct[index];
+        }
+    }
+    return null;
+}
+
+// return index if exist
+//  return -1 if not exist
+function checkItemInCart(id){
+    // console.log("listCartItem: ", listCartItem);
+    for (var index = 0; index < listCartItem.length; index++) {
+        if(id === Number(listCartItem[index].product.id)) {
+            // item existed!
+            // console.log("item existed index: ", index);
+            return index;
+        }
+    }
+    // not existed!
+    return -1;
+}
+
+
+function addToCart(id) {
+    // find product by id
+    var product = findProductById(id);
+    if(product == null){
+        alert("Product id " + id + " is NOT existed!!");
+        console.log("Product id " + id + " is NOT existed!!");
+    }
+    else {
+        // TODO: check item is existed in cart or not?
+        var index = checkItemInCart(id);
+        if(index >= 0) {
+            // increase quantity +1
+            listCartItem[index].quantity += 1;
+        }
+        else {
+            var cartItem = new CartItem(product, 1);
+            listCartItem.push(cartItem);
+        }
+        console.log(listCartItem);
+    }
+}
